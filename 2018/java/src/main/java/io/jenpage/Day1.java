dab30db4871ea773a4f866be7710778eaa5168dc
Jennifer <jenipage1989+github@gmail.com>
2018-12-04 09:07:53 -0500

    2018 Day 1 Part Two - Java

--- 2018/java/src/main/java/io/jenpage/Day1.java
+++ 2018/java/src/main/java/io/jenpage/Day1.java
@@ -2,8 +2,12 @@ package io.jenpage;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.function.IntFunction;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 import static java.nio.file.Files.lines;
 
@@ -12,10 +16,32 @@ public class Day1 {
         return deltas.sum();
     }
 
+    public static Optional<Integer> findRepeatFrequency( int[] deltas ) {
+        final HashMap<Integer, Integer> occurrances = new HashMap();
+        int currentFrequency = 0;
+        int currentOccurrances;
+
+        for (int i = 0; i < 1000; i++) {
+            for (int delta : deltas) {
+                currentOccurrances = Optional.ofNullable(occurrances.get(currentFrequency)).orElse(0) + 1;
+                if (currentOccurrances >= 2) {
+                    return Optional.of(currentFrequency);
+                }
+                occurrances.put(currentFrequency, currentOccurrances);
+                currentFrequency += delta;
+            }
+        }
+
+        return Optional.empty();
+    }
+
     public static void main( String[] args ) throws IOException {
         final Path path = Path.of(args[0]);
         final Stream<String> rawDeltas = lines(path);
         final IntStream deltas = rawDeltas.mapToInt((String delta) -> Integer.parseInt(delta));
         System.out.println( "Day 1 Part One Result: " + sumFrequencyDeltas(deltas) );
+
+        final int[] allDeltas = lines(path).mapToInt(Integer::parseInt).toArray();
+        System.out.println( "Day 1 Part Two Result: " + findRepeatFrequency(allDeltas).get() );
     }
 }
--- 2018/java/src/test/java/io/jenpage/Day1Test.java
+++ 2018/java/src/test/java/io/jenpage/Day1Test.java
@@ -30,4 +30,37 @@ public class Day1Test
         final IntStream deltas = rawDeltas.stream().mapToInt((String delta) -> Integer.parseInt(delta));
         assertEquals( -6, Day1.sumFrequencyDeltas(deltas) );
     }
+
+    @Test
+    public void p2example1() {
+        final List<String> rawDeltas = List.of("+1", "-1");
+        final int[] deltas = rawDeltas.stream().mapToInt(Integer::parseInt).toArray();
+        final int result = Day1.findRepeatFrequency(deltas).get();
+        assertEquals( 0, result );
+    }
+
+    @Test
+    public void p2example2() {
+        final List<String> rawDeltas = List.of("+3", "+3", "+4", "-2", "-4");
+        final int[] deltas = rawDeltas.stream().mapToInt(Integer::parseInt).toArray();
+        final int result = Day1.findRepeatFrequency(deltas).get();
+        assertEquals( 10, result );
+    }
+
+
+    @Test
+    public void p2example3() {
+        final List<String> rawDeltas = List.of("-6", "+3", "+8", "+5", "-6");
+        final int[] deltas = rawDeltas.stream().mapToInt(Integer::parseInt).toArray();
+        final int result = Day1.findRepeatFrequency(deltas).get();
+        assertEquals( 5, result );
+    }
+
+    @Test
+    public void p2example4() {
+        final List<String> rawDeltas = List.of("+7", "+7", "-2", "-7", "-4");
+        final int[] deltas = rawDeltas.stream().mapToInt(Integer::parseInt).toArray();
+        final int result = Day1.findRepeatFrequency(deltas).get();
+        assertEquals( 14, result );
+    }
 }

78cd12266530a5620aa70444a05a628c0645727f
Jennifer <jenipage1989+github@gmail.com>
2018-12-06 22:12:04 -0500

    2018 Day 3 Part 2 - java

--- 2018/java/src/main/java/io/jenpage/Day3.java
+++ 2018/java/src/main/java/io/jenpage/Day3.java
@@ -10,43 +10,63 @@ import java.util.HashMap;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.Arrays;
+import java.util.HashSet;
 
 import static java.nio.file.Files.lines;
 import static java.nio.file.Files.readAllLines;
 import static java.lang.Integer.parseInt;
 
 class Fabric {
-    private int[][] claims;
+    private String[][] claims;
+    private HashSet<String> clearIds;
 
     Fabric(int size) {
-        claims = new int[size][size];
+        claims = new String[size][size];
+        clearIds = new HashSet();
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                claims[i][j] = ".";
     }
 
-    public void makeClaim(int x, int y, int w, int h) {
+    public void makeClaim(String id, int x, int y, int w, int h) {
+        clearIds.add(id);
         for (int i = x; i < x + w; i++) {
             for (int j = y; j < y + h; j++) {
-                claims[i][j] += 1;
+                final String existing = claims[i][j];
+                if (existing.equals(".")) {
+                    claims[i][j] = id;
+                } else {
+                    claims[i][j] = "X";
+                    clearIds.remove(id);
+                    clearIds.remove(existing);
+                }
             }
         }
     }
 
     public void makeClaim(String claim) {
         final String[] parts = claim.split(" ");
+        final String id = parts[0].replace("#", "");
         final String[] location = parts[2].split(",");
         final String[] size = parts[3].split("x");
-        makeClaim(parseInt(location[0]), parseInt(location[1].replace(":", "")), parseInt(size[0]), parseInt(size[1]));
+        makeClaim(id, parseInt(location[0]), parseInt(location[1].replace(":", "")), parseInt(size[0]), parseInt(size[1]));
     }
 
     public int numOverlaps() {
         int overlaps = 0;
         for (int i = 0; i < claims.length; i++) {
             for (int j = 0; j < claims.length; j++) {
-                if (claims[i][j] > 1) {
+                if (claims[i][j].equals("X")) {
                     overlaps += 1;
                 }
             }
         }
         return overlaps;
+    }
+
+    public String nonOverlapping() {
+        return clearIds.iterator().next();
     }
 
     public void printClaims() {
@@ -69,9 +89,19 @@ public class Day3 {
         return fabric.numOverlaps();
     }
 
+    public static String nonOverlapping(int size, List<String> claims) {
+        Fabric fabric = new Fabric(size);
+        for (String claim : claims) {
+            fabric.makeClaim(claim);
+        }
+        return fabric.nonOverlapping();
+    }
+
     public static void main( String[] args ) throws IOException {
         final Path path = Path.of(args[0]);
         final List<String> claims = readAllLines(path);
         System.out.println( "Day 3 Part One Result: " + numOverlaps(1000, claims) );
+
+        System.out.println( "Day 3 Part Two Result: " + nonOverlapping(1000, claims) );
     }
 }
--- 2018/java/src/test/java/io/jenpage/Day3Test.java
+++ 2018/java/src/test/java/io/jenpage/Day3Test.java
@@ -15,4 +15,10 @@ public class Day3Test
         final List<String> claims = List.of("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2");
         assertEquals( 4, Day3.numOverlaps(8, claims) );
     }
+
+    @Test
+    public void part2Example() {
+        final List<String> claims = List.of("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2");
+        assertEquals( "3", Day3.nonOverlapping(8, claims) );
+    }
 }
